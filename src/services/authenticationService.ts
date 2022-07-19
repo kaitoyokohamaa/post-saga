fa6d2ec7c4a671ddf7bff8a569e5b60bf3893baf
kaitoyokohamaa <s1A121700885@toyo.jp>
2022-07-20 08:54:59 +0900

    feat: success sign up

--- src/services/authenticationService.ts
+++ src/services/authenticationService.ts
@@ -1,8 +1,10 @@
 export const registerUserService = async (request: any) => {
+  console.log(request.user);
   const REGISTER_API_ENDPOINT = `${process.env.NEXT_PUBLIC_API_KEY}/sign_up`;
   const parameters = {
     method: "POST",
     headers: {
+      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify(request.user),
