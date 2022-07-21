19fcf4f11d08a53e04763a188e67a6146496a6eb
kaitoyokohamaa <s1A121700885@toyo.jp>
2022-07-22 07:30:47 +0900

    feat: rename variable

--- src/saga/watcher.ts
+++ src/saga/watcher.ts
@@ -1,6 +1,6 @@
 import { takeLatest } from "redux-saga/effects";
 import { SIGNUP_USER } from "../ducks/signup/name";
-import { signupSaga } from "./authentication/authenticationSaga";
+import { signupSaga } from "./signup/signupSaga";
 export default function* watchUserAuthentication() {
   yield takeLatest(SIGNUP_USER, signupSaga);
 }
